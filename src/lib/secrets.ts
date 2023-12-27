c98cdbda8ee7a6d7b3ec2f4438367b689025edb0
Tom Planche <Tomplanche@icloud.com>
2023-12-27 14:39:15 +0100

    [12] Added good `secrets.ts` file

---
+++ src/lib/secrets.ts
@@ -0,0 +1,13 @@
+/**
+ * @file src/lib/secrets.ts
+ * @description secrets
+ * @author Tom Planche
+ */
+
+// VARIABLES ================================================================================================ VARIABLE
+export const LASTFM_API_KEY = 'haha_nope';
+// END VARIABLES ======================================================================================= END VARIABLES
+
+/**
+ * End of file src/lib/secrets.ts
+ */
